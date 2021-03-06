df6ce7b20647f38d0a4aa25d3cfc55caf75cd9f4
nemes <nemes@LAPTOP-JJ2TR7GJ>
2021-03-05 23:15:36 -0600

    version 1.1

---
+++ src/heloworld/HeloWorld.java
@@ -0,0 +1,13 @@
+//Eduardo Hernandez Lara 
+//2022822
+package heloworld;
+
+public class HeloWorld {
+    
+    public static void main(String[]args){
+        System.out.print("Hola java");
+    }
+    
+
+    
+}
